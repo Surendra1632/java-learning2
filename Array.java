2d28984f6842baa34a05ada50f6ba7ea31bd1bf4
Surendra1632 <surendra.lateral2k22@mpgi.edu.in>
2024-04-29 00:35:07 +0530

    arrays program

---
+++ Array.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        String fruits[] = { "apple", "boy", "cat", "dog" };
+        System.out.println(Arrays.toString(fruits));
+    }
+}
---
+++ arry1.java
@@ -0,0 +1,8 @@
+public class arry1 {
+    public static void main(String args[]){
+        int numbers []={1,2,3,4};
+        System.out.print(numbers[3]);
+
+    }
+    
+}
